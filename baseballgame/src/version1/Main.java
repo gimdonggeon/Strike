b9e0de22a62b66e2de7932667cb3e7cc28578e60
popcon1234 <popcon1234@naver.com>
2024-10-25 18:02:37 +0900

    feat : 야구게임 작동
    body : 아직 예외처리 미구현

--- baseballgame/src/version1/Main.java
+++ baseballgame/src/version1/Main.java
@@ -2,6 +2,9 @@ package version1;
 
 public class Main {
     public static void main(String[] args) {
-
+        RandomNumberGenerator rng = new RandomNumberGenerator();
+        BaseballGame bg = new BaseballGame();
+        System.out.println(rng.getAnswer());
+        bg.gameStart();
     }
 }
